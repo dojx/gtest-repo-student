828aa0506bac6455dc8cdf72ace37c07d2c5e429
Diego Jiménez <diego.o.jimz@gmail.com>
2024-11-26 10:58:50 -0600

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,15 +1,28 @@
 #include "StringCalculator.h"
-#include <cctype>
-#include <algorithm>
+
+int checkIsEmpty(string input) {
+  int result = -1;
+  if (input.empty()) {
+    result = 0;
+  }
+  return result;
+}
+
+int checkIsNotNumber(string input) {
+  int result = -1;
+  result = checkIsEmpty(input);
+  for (char ch : input) {
+    if (!isdigit(ch)) {
+      result = 0;
+      break;
+    }
+  }
+  return result;
+}
 
 int add(string input)
 {
-  if (input.empty()) {
-    return 0;
-  }
-  if (std::any_of(input.begin(), input.end(), [](char c) { return std::isalpha(c); }))
-  {
-    return 0;
-  } 
-  throw "invalid argument";
+  int result = -1;
+  result = checkIsEmpty(input) | checkIsNotEmpty(input);
+  return result;
 }
