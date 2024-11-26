cf19a71b2289d5defee2e076302de0fdb8921968
Diego Jiménez <diego.o.jimz@gmail.com>
2024-11-26 11:56:08 -0600

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,7 +1,7 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
-/* If there is no input then the output is 0 */
+/* 1. If there is no input then the output is 0 */
 TEST(StringCalculatorTestSuite, ExpectZeroForEmptyInput) 
 {
   string input = "";
@@ -10,11 +10,82 @@ TEST(StringCalculatorTestSuite, ExpectZeroForEmptyInput)
   ASSERT_EQ(actualValue, expectedValue);
 }
 
-/* If the input is characters, then the returned sum is 0. */
+/* 2. If the input is characters, then the returned sum is 0. */
 TEST(StringCalculatorTestSuite, ExpectZeroForCharacters) 
 {
   string input = "gokul";
   int expectedValue = 0;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+
+/* 3. If the given input string is a list of numbers separated by commas, the numbers are added up */
+TEST(StringCalculatorTestSuite, ExpectSumForTwoNumbers)
+{
+  string input = "1,2,3";
+  int expectedValue = 6;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+
+/* 4. If the given input string is a list of numbers separated by new line characters, the numbers are added up */
+TEST(StringCalculatorTestSuite, ExpectSumWithNewlineDelimite)
+{
+  string input = "1\n1\n2";
+  int expectedValue = 4;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+
+/* 5. If the given input consists of numbers greater than thousand then the number greater than 1000 should be ignored */
+TEST(StringCalculatorTestSuite, IgnoreNumbersGreaterThan1000)
+{
+  string input = "1, 1001";
+  int expectedValue = 1;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+
+/* 6. If the given input consists of delimiters the numbers are added up */
+TEST(StringCalculatorTestSuite, ExpectSumWithCustomDelimiter)
+{
+  string input = "//;\n1;2";
+  int expectedValue = 3;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+
+/* 7. If the give input is negative number then a warning should be given. */
+TEST(StringCalculatorTestSuite, ExpectExceptionforNegativeNumber)
+{
+  string input = "-7";
+  EXPECT_THROW({
+        try
+        {
+            add(input);
+        }
+        catch(const MyException& e)
+        {
+            EXPECT_STREQ("Negative not allowed", e.what());
+            throw;
+        }
+    }, MyException );
+}
+
+/* 8. If there is a single 0 then the output is 0 */
+TEST(StringCalculatorTestSuite, ExpectZeroForSingleZero)
+{
+  string input = "0";
+  int expectedValue = 0;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+
+/* 9. If the given input is equal to 1000 then the sum must be calculated. */
+TEST(StringCalculatorTestSuite, ExpectSumOfNumbersEqualto1000)
+{
+  string input = "999, 1000";
+  int expectedValue = 1999;
   int actualValue = add(input);
   ASSERT_EQ(actualValue, expectedValue);
 }
