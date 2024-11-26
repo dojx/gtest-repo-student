406d462105240603dd3bda3d5ca22965e5f2a937
Diego Jiménez <diego.o.jimz@gmail.com>
2024-11-26 10:43:13 -0600

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,9 +1,18 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
+/* 	If there is no input then the output is 0 */
 TEST(StringCalculatorTestSuite, ExpectZeroForEmptyInput) 
 {
   string input = "";
+  int expectedValue = 0;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
+/* If the input is characters, then the returned sum is 0. */
+TEST(StringCalculatorTestSuite, ExpectZeroForCharacters) 
+{
+  string input = "gokul";
   int expectedValue = 0;
   int actualValue = add(input);
   ASSERT_EQ(actualValue, expectedValue);
