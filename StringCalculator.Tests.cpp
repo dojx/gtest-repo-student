af0a1b00fc92fca0af083668cc096a15e90a4c7d
Diego Jiménez <diego.o.jimz@gmail.com>
2024-11-26 10:28:49 -0600

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,3 +1,10 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
+TEST(StringCalculatorTestSuite, ExpectZeroForEmptyInput) 
+{
+  string input = "";
+  int expectedValue = 0;
+  int actualValue = add(input);
+  ASSERT_EQ(actualValue, expectedValue);
+}
