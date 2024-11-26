480cec81783156f2bd1c8160c5b269b361453add
Diego Jiménez <diego.o.jimz@gmail.com>
2024-11-26 10:53:33 -0600

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -7,7 +7,7 @@ int add(string input)
   if (input.empty()) {
     return 0;
   }
-  if (any_of(begin(input), end(input), isalpha))
+  if (std::any_of(input.begin(), input.end(), [](char c) { return std::isalpha(c); })) {
   {
     return 0;
   } 
